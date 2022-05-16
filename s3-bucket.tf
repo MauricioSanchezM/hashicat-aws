25773b31e9ed2df400e771e29deec420287ebb68
babakDoraniArab <babak.dorani@gmail.com>
2022-05-16 11:53:58 +0000

    Added s3 bucket modul4e

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,10 @@
 module "s3-bucket" {
   source  = "app.terraform.io/demo-hashicorp-aws/s3-bucket/aws"
-  version = "3.2.0"
-  # insert required variables here
+  bucket = "my-s3-bucket-for-logs"
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  attach_elb_log_delivery_policy = true
 }
